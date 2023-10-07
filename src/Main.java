f27dba6da4496d07bedf63dee5aaa466af2f53db
mperehinets <mar.perehinets@gmail.com>
2023-10-08 00:29:36 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c = 9;
+        int d = 11;
+        int sumAB = a + b;
+        int sumCD = c + d;
+        System.out.println(sumAB > sumCD ? true : false);
+        sumAB++;
+        sumCD--;
+        sumCD--;
+        System.out.println(sumAB > sumCD ? true : false);
+        System.out.println(sumAB % 2 == 0 || sumCD % 2 == 0 ? true : false);
+    }
+}
